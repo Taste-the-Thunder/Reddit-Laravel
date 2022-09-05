27661162ff8a2b4cef0777d92ff043e62b48c5ad
Dhruvin <35141691+dhruvinprajapati@users.noreply.github.com>
2022-09-05 13:19:48 +0530

    we can store the communities on our database with all validation

--- app/Http/Requests/CommunityStoreRequest.php
+++ app/Http/Requests/CommunityStoreRequest.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Requests;
 
+use App\Models\Community;
 use Illuminate\Foundation\Http\FormRequest;
 
 class CommunityStoreRequest extends FormRequest
@@ -13,7 +14,7 @@ class CommunityStoreRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +25,9 @@ class CommunityStoreRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'name' => ['required', 'unique:communities'],
+            'description' => ['required', 'min:5'],
+            'slug' => ['required', 'unique:communities']
         ];
     }
 }

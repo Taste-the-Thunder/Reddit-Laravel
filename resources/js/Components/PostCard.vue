b243c47df49db0ff477e539d72d9e6f9cbfd1281
Dhruvin <35141691+dhruvinprajapati@users.noreply.github.com>
2022-09-06 12:46:49 +0530

    display evry post of perticular community

---
+++ resources/js/Components/PostCard.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="p-2 max-w-4xl flex bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
+        <div class="mr-3">upvote</div>
+        <div>
+            <div class="flex m-2 p-2">
+                <span class="font-semibold mr-3">r/{{ community.name }}</span>
+                <div class="flex">
+                    Posted By
+                    <span class="font-semibold ml-1">{{ post.username }}</span>
+                </div>
+            </div>
+            <a href="">
+                <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ post.title }}</h5>
+            </a>
+            <p class="mb-3 font-normal text-gray-300 dark:text-gray-400">{{ post.description }}</p>
+            <div class="flex m-2 p-2">
+                <p class="mr-4 p-2">Comments(2)</p>
+                <a href="" class="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-500 rounded">
+                    Read More
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+    defineProps({
+        post:Object,
+        community:Object,
+    });
+</script>
--- resources/js/Pages/Frontend/Communities/Show.vue
+++ resources/js/Pages/Frontend/Communities/Show.vue
@@ -1,18 +1,26 @@
 <template>
     <guest-layout>
-        <div class="flex justify-end m-2 p-2">
-            <Link v-if="$page.props.auth.auth_check" :href="route('communities.posts.create', community.slug)" class="px-3 py-4 rounded bg-indigo-500 hover:bg-indigo-900 text-white">Create Post</Link>
-
-        </div>
-        <h1>Subreddit {{ community.name }} page</h1>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+                r/{{ community.name }}
+            </h2>
+            <div class="flex justify-end m-2 p-2">
+                <Link v-if="$page.props.auth.auth_check" :href="route('communities.posts.create', community.slug)" class="px-3 py-4 rounded bg-indigo-500 hover:bg-indigo-900 text-white">Create Post</Link>
+            </div>
+        </template>
+        <section class="m-2 p-2">
+            <PostCard v-for="post in posts.data" :post="post" :community="community" :key="post.id"></PostCard>
+        </section>
     </guest-layout>
 </template>
 
 <script setup>
     import GuestLayout from '@/Layouts/Guest.vue';
     import { Link } from '@inertiajs/inertia-vue3';
+    import PostCard from '@/Components/PostCard.vue';
     defineProps({
-        community:Object
+        community:Object,
+        posts:Object
     });
 
 </script>
